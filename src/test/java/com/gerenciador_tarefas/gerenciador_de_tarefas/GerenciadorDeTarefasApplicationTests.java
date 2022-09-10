eacd5798e2ac69fa143b23b0417e378f873af3c7
Thauan Amorim <thauanssussu@gmail.com>
2022-09-10 15:09:27 -0300

    commit inicial

---
+++ src/test/java/com/gerenciador_tarefas/gerenciador_de_tarefas/GerenciadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador_tarefas.gerenciador_de_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
